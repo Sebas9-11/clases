2a56328ecd352ac4db6b787b4e1d157b6a075664
Andres Barrios <sebastian-1999@outlook.com>
2024-10-19 16:52:49 -0500

    clase 1

---
+++ arrays/app.js
@@ -0,0 +1,24 @@
+const frutas = ["banano", "pera", "mango", "manzana", "uva"];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const booleans = [true, false, true, false, false];
+
+// if(condicion) {
+//   console.log("mensaje")
+// } else {
+//  console.log("mensaje altertivo")
+// }
+
+//ciclo for
+// for(let i=0; i<= largo de la coleccion; i++){
+// conleccion[i]
+// }
+
+// for (let i = 0; i < booleans.length; i++) {
+//   if (booleans[i]) {
+//     console.log(booleans[i]);
+//   }
+// }
+
+for (let i = 2; i <= 1000; i += 2) {
+  console.log(i);
+}
